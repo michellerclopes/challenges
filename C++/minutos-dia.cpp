c82e18616eb66d153b6aaacd915e3fcf6703bf97
Michelle Lopes <michellerclopes@gmail.com>
2018-03-25 12:13:00 -0300

    to do

--- C++/minutos-dia.cpp
+++ C++/minutos-dia.cpp
@@ -1,1 +1,3 @@
 //Faça um algoritmo que leia um valor de hora e informe quantos minutos se passaram desde o início do dia.
+
+
---
+++ C++/troca-valor.cpp
@@ -0,0 +1,3 @@
+/* Ler dois valores para as variáveis A e B, efetuar a troca dos valores de forma que a variável A passe a possuir o valor da variável B 
+e que a variável B passe a possuir o valor da variável A. Apresentar os valores trocados. */
+
