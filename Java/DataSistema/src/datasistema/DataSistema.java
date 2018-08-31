f8c38b13a28c0e49e6794e8cae0ae32d86fcf91f
michellerclopes <michellerclopes>
2018-08-31 00:22:33 -0300

    studies

---
+++ Java/DataSistema/src/datasistema/DataSistema.java
@@ -0,0 +1,13 @@
+package datasistema;
+
+import java.util.Date;
+
+public class DataSistema {
+
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A data do sistema é");
+        System.out.println(relogio.toString());
+    }
+    
+}
---
+++ Java/IdiomaSistema/src/idiomasistema/IdiomaSistema.java
@@ -0,0 +1,12 @@
+package idiomasistema;
+
+public class IdiomaSistema {
+
+
+    public static void main(String[] args) {
+        System.getProperties();
+        System.out.print("Idioma do SO: ");
+        System.out.println(System.getProperty("user.language"));
+    }
+    
+}
